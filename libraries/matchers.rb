aad4d718e8a21d47d085443952022a2611534b0a
jtimberman <joshua@chef.io>
2015-12-14 22:19:29 -0700

    matchers for other actions on samba_user

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,4 +2,12 @@ if defined? ChefSpec
   def create_samba_user(name)
     ChefSpec::Matchers::ResourceMatcher.new :samba_user, :create, name
   end
+
+  def enable_samba_user(name)
+    ChefSpec::Matchers::ResourceMatcher.new :samba_user, :enable, name
+  end
+
+  def delete_samba_user(name)
+    ChefSpec::Matchers::ResourceMatcher.new :samba_user, :delete, name
+  end
 end
