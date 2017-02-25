4d92409c6739054bc0372700a0f71633d4ab2bc7
Dan Webb <dan.webb@damacus.io>
2017-02-25 17:56:21 +0000

    Require chef 12.5

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache 2.0'
 description      'Installs/Configures samba'
-version          '1.0.2'
+version          '1.0.3'
 
 %w( debian ubuntu centos fedora redhat scientific amazon oracle ).each do |os|
   supports os
@@ -11,4 +11,4 @@ end
 
 source_url 'https://github.com/sous-chefs/samba'
 issues_url 'https://github.com/sous-chefs/samba/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.5' 
