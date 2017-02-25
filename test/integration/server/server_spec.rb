b687610181c2b98a699e6eb6e3293698e1ece754
Dan Webb <dan.webb@damacus.io>
2017-02-25 17:55:17 +0000

    Appease the cookstyle gods

--- test/integration/server/server_spec.rb
+++ test/integration/server/server_spec.rb
@@ -41,7 +41,7 @@ describe file('/etc/samba/smb.conf') do
   its('content') { should match /security = user/ }
   its('content') { should match /map to guest = Bad User/ }
   its('content') { should match /interfaces = lo 127.0.0.1 enp\* eth\*/ }
-  its('content') { should match /hosts allow = 192.168.1.0\/24/ }
+  its('content') { should match %r{hosts allow = 192.168.1.0\/24} }
   its('content') { should match /load printers = no/ }
   its('content') { should match /passdb backend = tdbsam/ }
   its('content') { should match /dns proxy = no/ }
