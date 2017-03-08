87c2c6c3f6d4c3fc37857225cc6539aca676c16c
Dan Webb <dan.webb@damacus.io>
2017-03-08 18:14:37 +0000

    Add support for older Chef versions that don't support source/issues and chef_version

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ version          '1.0.3'
   supports os
 end
 
-source_url 'https://github.com/sous-chefs/samba'
-issues_url 'https://github.com/sous-chefs/samba/issues'
-chef_version '>= 12.5'
+source_url 'https://github.com/sous-chefs/samba' if respond_to?(:source_url)
+issues_url 'https://github.com/sous-chefs/samba/issues' if respond_to?(:issues_url)
+chef_version '>= 12.5' if respond_to?(:chef_version)
