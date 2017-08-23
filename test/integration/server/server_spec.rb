4e08fc2ead8a826c273bcbb897b3932da21c4382
Erinn D Looney-Triggs <erinn.looneytriggs@gmail.com>
2017-08-23 10:54:59 -0400

    Modify template and test for winbind separator.

--- test/integration/server/server_spec.rb
+++ test/integration/server/server_spec.rb
@@ -47,7 +47,6 @@ describe file('/etc/samba/smb.conf') do
   its('content') { should match(/realm = $/) }
   its('content') { should match(/password server = \*$/) }
   its('content') { should match(/kerberos method = secrets only$/) }
-  its('content') { should match(/winbind separator = \\/) }
   its('content') { should match(/workgroup = HOME/) }
   its('content') { should match(/server string = fat/) }
   its('content') { should match(/security = user/) }
