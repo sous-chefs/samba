7da06da90659afaa453ec3074c95218beace55d9
Dan Webb <dan.webb@damacus.io>
2018-02-19 20:43:15 +0000

    I don't understand how this even works in travis

--- test/integration/server/server_spec.rb
+++ test/integration/server/server_spec.rb
@@ -19,6 +19,13 @@ describe directory('/home/test_user_1') do
   it { should exist }
 end
 
+salt = command('grep test_user_1 /etc/shadow | awk -F\'$\' \'{ print $3 }\'').stdout.strip
+password_string = 'superawesomepassword'.crypt('$6$' + salt)
+
+describe shadow.users('test_user_1') do
+  its('passwords') { should cmp [password_string] }
+end
+
 describe user('test_user_2') do
   it { should exist }
 end
