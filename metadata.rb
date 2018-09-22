4380828a875aa39698b97e4d5020f39036ba891b
Dan Webb <dan.webb@damacus.io>
2018-09-22 18:36:27 +0100

    Add ubuntu-18.04 and fedora-latest

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs/Configures samba'
-version          '1.1.4'
+version          '1.2.0'
 source_url       'https://github.com/sous-chefs/samba'
 issues_url       'https://github.com/sous-chefs/samba/issues'
-chef_version     '>= 12.16' if respond_to?(:chef_version)
+chef_version     '>= 13.0' if respond_to?(:chef_version)
 
 %w(debian ubuntu centos fedora redhat scientific amazon oracle).each do |os|
   supports os
